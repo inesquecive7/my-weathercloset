580358c12be21027880006002de2a4535e07a124
inesquecive7 <51125561+inesquecive7@users.noreply.github.com>
2019-07-22 10:54:21 +0100

    style closet component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import Closet from "./Closet.js";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Closet city="Lisbon" />
+      <div className="container">
+        <div className="card m-3 p-3">
+          <div className="card-body">
+            <Closet city="Lisbon" />
+          </div>
+        </div>
       </div>
     );
   }
--- src/Closet.js
+++ src/Closet.js
@@ -1,22 +1,37 @@
 import React, { Component } from "react";
-
+import "./Closet.css";
 class Closet extends Component {
   render() {
     return (
       <div>
         <h1>Weather Closet</h1>
-        <h3>{this.props.city}</h3>
+        <h2>{this.props.city}</h2>
         <ul>
           <li>Monday, July 22</li>
           <li>Very sunny</li>
         </ul>
-        <div className="temperature">
-          30 <small>º C</small>
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="clearfix">
+              {" "}
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                alt=""
+                className="float-left"
+              />
+              <div className="temperature float-left">
+                30 <small>º C | F</small>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-6">
+            <ul>
+              <li>Humidity: 26 %</li>
+              <li>Wind speed: 5.2 kmH</li>
+            </ul>
+          </div>
         </div>
-        <ul>
-          <li>Humidity: 26%July</li>
-          <li>Wind speed: 5.2 kmH</li>
-        </ul>
       </div>
     );
   }
